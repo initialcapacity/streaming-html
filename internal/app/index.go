ebb229c4fda3fef41893bab624732d1cadd44ba0
Tyson Gern <tyson@initialcapacity.io>
2024-03-17 11:03:47 -0600

    First pass at partially rendering a template

--- internal/app/index.go
+++ internal/app/index.go
@@ -8,15 +8,27 @@ import (
 
 func Index(addArtificialDelay bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		websupport.Render(w, Resources, "index", nil)
-		websupport.Flush(w)
+		c := make(chan []string)
 
-		if addArtificialDelay {
-			time.Sleep(2 * time.Second)
-		}
+		go func() {
+			if addArtificialDelay {
+				time.Sleep(2 * time.Second)
+			}
+			items := []string{
+				"Here's some slower content.",
+				"It took a while to load.",
+				"And didn't use any javascript.",
+			}
+			c <- items
+			websupport.Flush(w)
+			close(c)
+		}()
 
-		websupport.Render(w, Resources, "content", nil)
-		websupport.Render(w, Resources, "end", nil)
-		websupport.Flush(w)
+		go func() {
+			time.Sleep(10 * time.Millisecond)
+			websupport.Flush(w)
+		}()
+
+		websupport.Render(w, Resources, "index", c)
 	}
 }
