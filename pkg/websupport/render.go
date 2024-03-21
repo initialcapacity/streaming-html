0137f139b2df7069da4d93e244eeceeadd02641b
Tyson Gern <tyson@initialcapacity.io>
2024-03-21 14:20:47 -0600

    Set transfer encoding

--- pkg/websupport/render.go
+++ pkg/websupport/render.go
@@ -11,6 +11,7 @@ func Render(writer http.ResponseWriter, resources fs.FS, templateName string, da
 	fileName := fmt.Sprintf("%s.gohtml", templateName)
 	flushWriter := NewFlushWriter(writer)
 
+	writer.Header().Set("Transfer-Encoding", "chunked")
 	return template.Must(template.New(fileName).
 		ParseFS(resources, "resources/templates/template.gohtml", fmt.Sprintf("resources/templates/%s", fileName))).
 		Execute(flushWriter, data)
