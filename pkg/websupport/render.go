75d36bc7221450b73152a1e452c3bc6942a6b052
Tyson Gern <tyson@initialcapacity.io>
2024-03-21 21:28:37 -0600

    Use defer in template instead of flush writer

--- pkg/websupport/render.go
+++ pkg/websupport/render.go
@@ -9,10 +9,19 @@ import (
 
 func Render(writer http.ResponseWriter, resources fs.FS, templateName string, data any) error {
 	fileName := fmt.Sprintf("%s.gohtml", templateName)
-	flushWriter := NewFlushWriter(writer)
 
 	writer.Header().Set("Transfer-Encoding", "chunked")
 	return template.Must(template.New(fileName).
+		Funcs(template.FuncMap{
+			"defer": func(value any) (any, error) {
+				err := flush(writer)
+				return value, err
+			},
+		}).
 		ParseFS(resources, "resources/templates/template.gohtml", fmt.Sprintf("resources/templates/%s", fileName))).
-		Execute(flushWriter, data)
+		Execute(writer, data)
+}
+
+func flush(writer http.ResponseWriter) error {
+	return http.NewResponseController(writer).Flush()
 }
