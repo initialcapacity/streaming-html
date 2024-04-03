60373d5104f38e548dcd614c432c3970c33859c6
Tyson Gern <tyson@initialcapacity.io>
2024-04-03 16:35:15 -0600

    Test streaming behavior in handler

--- internal/app/index_test.go
+++ internal/app/index_test.go
@@ -7,17 +7,41 @@ import (
 	"io"
 	"net/http"
 	"testing"
+	"time"
 )
 
+type controlledWaiter struct {
+	C chan time.Time
+}
+
+func (w controlledWaiter) Wait() <-chan time.Time {
+	return w.C
+}
+
 func TestIndex(t *testing.T) {
-	address, server := testsupport.StartTestServer(t, app.Handlers(immediateWaiter{}))
+	delayChannel := make(chan time.Time)
+	waiter := controlledWaiter{C: delayChannel}
+
+	address, server := testsupport.StartTestServer(t, app.Handlers(waiter))
 	defer testsupport.StopTestServer(t, server)
 
 	response, err := http.Get(address)
 	assert.NoError(t, err)
 
-	body, err := io.ReadAll(response.Body)
+	initialRead := readString(t, response.Body)
+	assert.Contains(t, initialRead, "Streaming HTML")
+	assert.NotContains(t, initialRead, "Success!")
+
+	delayChannel <- time.Now()
+
+	finalRead, err := io.ReadAll(response.Body)
 	assert.NoError(t, err)
-	assert.Contains(t, string(body), "Streaming HTML")
-	assert.Contains(t, string(body), "Success!")
+	assert.Contains(t, string(finalRead), "Success!")
+}
+
+func readString(t *testing.T, reader io.Reader) string {
+	firstRead := make([]byte, 0, 4096)
+	n, err := reader.Read(firstRead[0:4096])
+	assert.NoError(t, err)
+	return string(firstRead[:len(firstRead)+n])
 }
